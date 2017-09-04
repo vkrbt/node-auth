1fc4fe64a48d9edf647a0bb7263cda968b1f1822
Vladislav Korbut <v.korbut8@gmail.com>
2017-09-04 19:23:01 +0300

    anonim messages draft

--- app/sockets.js
+++ app/sockets.js
@@ -2,8 +2,8 @@ module.exports = (io) => {
   io.on('connection', (socket) => {
     socket.emit('connected', 'Yeah');
 
-    socket.on('authenticate', (data) => {
-      console.log(data);
+    socket.on('message', (data) => {
+      socket.emit('message', data);
     });
   });
 };
