99fcecee794a620d09752e5f3d51cfbc6b768f55
Vladislav Korbut <v.korbut8@gmail.com>
2017-09-05 11:06:26 +0300

    socket.io auth

--- app/sockets.js
+++ app/sockets.js
@@ -1,9 +1,38 @@
-module.exports = (io) => {
-  io.on('connection', (socket) => {
-    socket.emit('connected', 'Yeah');
+const jwt = require('jsonwebtoken');
+const options = require('./config');
+const MessageSchema = require('./models/message.model');
 
-    socket.on('message', (data) => {
-      socket.emit('message', data);
+const authenticate = (socket, next) => {
+  const token = socket.handshake.query.token;
+  const user = jwt.decode(token.slice(4), options.secretOrKey);
+  /* es-lint-disable no-param-reassign */
+  socket.handshake.user = user;
+  /* eslint-enable */
+  next();
+};
+
+module.exports = (io) => {
+  io.use(authenticate);
+
+  io.on('connection', (socket) => {
+    socket.on('message', async (msg) => {
+      const messageObject = {
+        text: msg.trim(),
+        userId: socket.handshake.user.id,
+      };
+
+      const message = await MessageSchema.create(messageObject);
+      console.log(message);
+    });
+
+    socket.on('history', async () => {
+      const messages = await MessageSchema
+        .find({})
+        .sort({ date: -1 })
+        .limit(50)
+        .sort({ date: 1 })
+        .lean();
+      socket.emit('history', messages);
     });
   });
 };
